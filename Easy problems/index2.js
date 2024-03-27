f770adb5970315d1a0d5c045e7880564ad5eb1eb
Nikki0830 <gniki0830@gmail.com>
2024-03-27 11:37:29 +0530

    Find duplicates from a sorted array

---
+++ Easy problems/index2.js
@@ -0,0 +1,14 @@
+//find duplicates from an sorted array
+var removeDuplicates = function (nums) {
+  let i = nums.length;
+
+  while (0 < nums.length) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+    }
+    i++;
+  }
+  return nums.length;
+};
+console.log(removeDuplicates([1, 1, 2]));
+// removeDuplicates([1, 1, 2, 2, 2]);
