27e5784100c3739e8f41403950b6ad8b86fa3762
Nikki0830 <gniki0830@gmail.com>
2024-03-27 11:35:34 +0530

    Find sum of indeces of a target value

---
+++ Easy problems/index1.js
@@ -0,0 +1,15 @@
+//find sum of indeces of a target value
+
+// var nums = [2,7,11,15]
+// var target = 9;
+
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+console.log(twoSum([2,7,11,15],9))
